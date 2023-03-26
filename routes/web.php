3efc0c0f1f11ab86b4c334b5e67836f83cf98247
川﨑 亜美 <a-kawasaki065@jutjoy.com>
2023-03-26 17:29:50 +0900

    LARAVEL_1-129 4-12 Vite

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Admin\NewsController;
+
+use App\Http\Controllers\Admin\SelfProfileController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::controller(NewsController::class)->prefix('admin')->name('news.')->group(function () {
+    Route::get('news/create', 'add')->name('add');
+});
+
+Route::controller(SelfProfileController::class)->prefix('admin')->name('profile.')->group(function () {
+    Route::get('profile/create', 'add')->name('add');
+    Route::get('profile/edit', 'edit')->name('edit');
+});
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/sass/admin.scss',
+                'resources/sass/profile.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
